81634de3536facbefaab5ad281b327161630e195
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-08-20 16:24:46 +0700

    Initial set up counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    const reset = () => setCount(0);
+    const color = count > 0 ? "green" : count < 0 ? "red" : "black";
+    console.log(count > 0, "color:", color);
+
+    return (
+        <div className="container">
+            <span> Project Counter </span>
+            <div className="mainBoard">
+                <div id="title">Counter</div>
+                <div id="counter" style={{ color: color }}>{count}</div>
+                <div id="action">
+                    <button onClick={decrement} className="btn" id="btn-decrease">Decrease</button>
+                    <button onClick={reset} className="btn" id="btn-reset">Reset</button>
+                    <button onClick={increment} className="btn" id="btn-increase">Increase</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
